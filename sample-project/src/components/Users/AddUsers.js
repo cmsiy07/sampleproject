fe38f6a5ca4fdc8cbf089cceccf98081ef19719c
Cheryl Siy <cherylmsiy@gmail.com>
2021-12-14 15:42:13 +0800

    AddUser Component

---
+++ sample-project/src/components/Users/AddUsers.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+
+const AddUsers = (props) => {
+
+const addUserHandler = (event) =>{
+  event.preventDefault();
+  console.log("add user")
+}
+
+  return (
+    <form onSubmit={addUserHandler}>
+      <label htmlFor="username">Username</label>
+      <input id="username" type="text"></input>
+      <label htmlFor="age">Age(Years)</label>
+      <input id="age" type="number"></input>
+      <button type="submit">Add User</button>
+    </form>
+  );
+  
+
+}
+
+export default AddUsers
