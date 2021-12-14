8ac8d5d6156cec9eeac2cfadcd40b42001be6e61
Cheryl Siy <cherylmsiy@gmail.com>
2021-12-14 15:57:47 +0800

    added css modules

---
+++ sample-project/src/components/UI/Card.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import style from './Card.module.css'
+
+const Card = (props) => {
+  return <div className = {`${style.card} ${props.className}`}>  {props.children}</div>;
+};
+
+export default Card;
--- sample-project/src/components/Users/AddUsers.js
+++ sample-project/src/components/Users/AddUsers.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import Card from '../UI/Card.js'
 
+import styles from './AddUsers.module.css'
 
 const AddUsers = (props) => {
 
@@ -9,13 +11,15 @@ const addUserHandler = (event) =>{
 }
 
   return (
-    <form onSubmit={addUserHandler}>
-      <label htmlFor="username">Username</label>
-      <input id="username" type="text"></input>
-      <label htmlFor="age">Age(Years)</label>
-      <input id="age" type="number"></input>
-      <button type="submit">Add User</button>
-    </form>
+    <Card className= {styles.input}>
+      <form onSubmit={addUserHandler}>
+        <label htmlFor="username">Username</label>
+        <input id="username" type="text"></input>
+        <label htmlFor="age">Age(Years)</label>
+        <input id="age" type="number"></input>
+        <button type="submit">Add User</button>
+      </form>
+    </Card>
   );
   
 
